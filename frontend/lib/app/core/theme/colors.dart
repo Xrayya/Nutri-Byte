a85098506abfa575f1c32d83db3b63966b433eab
Xrayya <nur.alkhiyar@gmail.com>
2024-01-08 23:31:52 +0700

    feat(theme): define color palette

---
+++ frontend/lib/app/core/theme/colors.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class NutriByteColor {
+  static final _KeyColors keyColors = _KeyColors();
+  static final _Schemes schemas = _Schemes();
+}
+
+class _KeyColors {
+  final Color primary = const Color(0xFF65B741);
+  final Color secondary = const Color(0xFFC1F2B0);
+  final Color tetriary = const Color(0xFFFFB534);
+  final Color neutral = const Color(0xFFFBF6EE);
+  final Color neutralVariant = const Color(0xFFEEF0E5);
+}
+
+class _Schemes {
+  final Color primary = const Color(0xFF446832);
+  final Color surfaceTint = const Color(0xFF446832);
+  final Color onPrimary = const Color(0xFFFFFFFF);
+  final Color primaryContainer = const Color(0xFFC4EFAC);
+  final Color onPrimaryContainer = const Color(0xFF062100);
+  final Color secondary = const Color(0xFF406836);
+  final Color onSecondary = const Color(0xFFFFFFFF);
+  final Color secondaryContainer = const Color(0xFFC0EFB0);
+  final Color onSecondaryContainer = const Color(0xFF002200);
+  final Color tertiary = const Color(0xFF7E570F);
+  final Color onTertiary = const Color(0xFFFFFFFF);
+  final Color tertiaryContainer = const Color(0xFFFFDDB0);
+  final Color onTertiaryContainer = const Color(0xFF291800);
+  final Color error = const Color(0xFFBA1A1A);
+  final Color onError = const Color(0xFFFFFFFF);
+  final Color errorContainer = const Color(0xFFFFDAD6);
+  final Color onErrorContainer = const Color(0xFF410002);
+  final Color background = const Color(0xFFF8FAF0);
+  final Color onBackground = const Color(0xFF191D16);
+  final Color surface = const Color(0xFFF8FAF0);
+  final Color onSurface = const Color(0xFF191D16);
+  final Color surfaceVariant = const Color(0xFFDFE4D7);
+  final Color onSurfaceVariant = const Color(0xFF43483E);
+  final Color outline = const Color(0xFF73796D);
+  final Color outlineVariant = const Color(0xFFC3C8BB);
+  final Color shadow = const Color(0xFF000000);
+  final Color scrim = const Color(0xFF000000);
+  final Color inverseSurface = const Color(0xFF2E312B);
+  final Color inverseOnSurface = const Color(0xFFEFF2E7);
+  final Color inversePrimary = const Color(0xFFA9D292);
+  final Color primaryFixed = const Color(0xFFC4EFAC);
+  final Color onPrimaryFixed = const Color(0xFF062100);
+  final Color primaryFixedDim = const Color(0xFFA9D292);
+  final Color onPrimaryFixedVariant = const Color(0xFF2D4F1D);
+  final Color secondaryFixed = const Color(0xFFC0EFB0);
+  final Color onSecondaryFixed = const Color(0xFF002200);
+  final Color secondaryFixedDim = const Color(0xFFA5D396);
+  final Color onSecondaryFixedVariant = const Color(0xFF285020);
+  final Color tertiaryFixed = const Color(0xFFFFDDB0);
+  final Color onTertiaryFixed = const Color(0xFF291800);
+  final Color tertiaryFixedDim = const Color(0xFFF2BD6E);
+  final Color onTertiaryFixedVariant = const Color(0xFF614000);
+  final Color surfaceDim = const Color(0xFFD9DBD1);
+  final Color surfaceBright = const Color(0xFFF8FAF0);
+  final Color surfaceContainerLowest = const Color(0xFFFFFFFF);
+  final Color surfaceContainerLow = const Color(0xFFF2F5EA);
+  final Color surfaceContainer = const Color(0xFFEDEFE5);
+  final Color surfaceContainerHigh = const Color(0xFFE7E9DF);
+  final Color surfaceContainerHighest = const Color(0xFFE1E4D9);
+}
---
+++ frontend/lib/app/core/theme/theme_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:nutri_byte/app/core/theme/colors.dart';
+
+ThemeData nutriByteTheme() {
+  return ThemeData(
+  colorSchemeSeed: NutriByteColor.keyColors.primary,
+    useMaterial3: true,
+  );
+}
