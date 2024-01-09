cdb3c5eb91417d8f680137c9030fc3c7c0a05ede
Xrayya <nur.alkhiyar@gmail.com>
2024-01-09 16:38:27 +0700

    feat(config): update theme and color

--- frontend/lib/app/core/theme/theme_data.dart
+++ frontend/lib/app/core/theme/theme_data.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:nutri_byte/app/core/theme/colors.dart';
 
-ThemeData nutriByteTheme() {
-  return ThemeData(
-    colorSchemeSeed: NutriByteColor.keyColors.primary,
-    useMaterial3: true,
-  );
+class NutriByteTheme {
+  static ThemeData lightTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: NutriByteColor.seedColor,
+          primary: NutriByteColor.light.primary,
+          secondary: NutriByteColor.light.secondary,
+          tertiary: NutriByteColor.light.tertiary,
+          onTertiary: NutriByteColor.light.onTertiary,
+          tertiaryContainer: NutriByteColor.light.tertiaryContainer,
+          onTertiaryContainer: NutriByteColor.light.onTertiaryContainer,
+          brightness: Brightness.light)
+      );
+
+  static ThemeData darkTheme = ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: NutriByteColor.seedColor,
+          primary: NutriByteColor.dark.primary,
+          secondary: NutriByteColor.dark.secondary,
+          tertiary: NutriByteColor.dark.tertiary,
+          onTertiary: NutriByteColor.dark.onTertiary,
+          tertiaryContainer: NutriByteColor.dark.tertiaryContainer,
+          onTertiaryContainer: NutriByteColor.dark.onTertiaryContainer,
+          brightness: Brightness.dark)
+      );
 }
