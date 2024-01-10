eb9234b3dacada813b4afcc8e981c34c2955d368
Xrayya <nur.alkhiyar@gmail.com>
2024-01-10 23:19:14 +0700

    chore: remove unused var

--- frontend/lib/app/modules/home/dashboard/views/dashboard_view.dart
+++ frontend/lib/app/modules/home/dashboard/views/dashboard_view.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:nutri_byte/app/data/models.dart';
 
 import '../controllers/dashboard_controller.dart';
-
-var pieData = <ChartData>[
-  ChartData("Carbs", 98, 400),
-  ChartData("Protein", 200, 250),
-  ChartData("Fat", 20, 40)
-];
 
 class DashboardView extends GetView<DashboardController> {
   const DashboardView({Key? key}) : super(key: key);
