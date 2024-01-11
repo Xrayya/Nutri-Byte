f6b76bd10f8412509e733d86609ffeb0766ae753
kyhchn <kyaahh69@gmail.com>
2024-01-12 06:42:12 +0700

    feat: theming

--- frontend/lib/app/core/theme/theme_data.dart
+++ frontend/lib/app/core/theme/theme_data.dart
@@ -3,18 +3,139 @@ import 'package:nutri_byte/app/core/theme/colors.dart';
 
 class NutriByteTheme {
   static ThemeData lightTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: "Signika",
-      colorScheme: ColorScheme.fromSeed(
-          seedColor: NutriByteColor.seedColor,
-          primary: NutriByteColor.light.primary,
-          secondary: NutriByteColor.light.secondary,
-          tertiary: NutriByteColor.light.tertiary,
-          onTertiary: NutriByteColor.light.onTertiary,
-          tertiaryContainer: NutriByteColor.light.tertiaryContainer,
-          onTertiaryContainer: NutriByteColor.light.onTertiaryContainer,
-          brightness: Brightness.light)
-      );
+    useMaterial3: true,
+    fontFamily: "Signika",
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+          fontSize: 57, fontFamily: 'Signika', fontWeight: FontWeight.w400),
+      displayMedium: TextStyle(
+        fontSize: 45,
+        fontWeight: FontWeight.w400,
+      ),
+      displaySmall: TextStyle(
+        fontSize: 36,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Signika',
+      ),
+      headlineLarge: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Signika',
+      ),
+      headlineMedium: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Signika',
+      ),
+      headlineSmall: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Signika',
+      ),
+      titleLarge: TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.w600,
+        fontFamily: 'Signika',
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Signika',
+        letterSpacing: 0.15,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Signika',
+        letterSpacing: 0.1,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Signika',
+        letterSpacing: 0.1,
+      ),
+      labelMedium: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Signika',
+        letterSpacing: 0.5,
+      ),
+      labelSmall: TextStyle(
+        fontSize: 11,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Signika',
+        letterSpacing: 0.5,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Signika',
+        letterSpacing: 0.5,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Signika',
+        letterSpacing: 0.25,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Signika',
+        letterSpacing: 0.4,
+      ),
+    ),
+    colorScheme: ColorScheme.fromSeed(
+        seedColor: NutriByteColor.seedColor,
+        primary: NutriByteColor.light.primary,
+        secondary: NutriByteColor.light.secondary,
+        tertiary: NutriByteColor.light.tertiary,
+        primaryContainer: NutriByteColor.light.primaryContainer,
+        background: NutriByteColor.light.background,
+        surfaceTint: NutriByteColor.light.surfaceTint,
+        onTertiary: NutriByteColor.light.onTertiary,
+        tertiaryContainer: NutriByteColor.light.tertiaryContainer,
+        onTertiaryContainer: NutriByteColor.light.onTertiaryContainer,
+        outline: NutriByteColor.light.outline,
+        brightness: Brightness.light),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(16),
+      alignLabelWithHint: true,
+      focusColor: NutriByteColor.light.primary,
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: NutriByteColor.light.outline, width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: NutriByteColor.light.primary, width: 3),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: const BorderSide(color: Colors.red, width: 1),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: const BorderSide(color: Colors.red, width: 3),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStateProperty.all(
+          const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Signika',
+            letterSpacing: 0.5,
+          ),
+        ),
+        foregroundColor:
+            MaterialStateProperty.all(NutriByteColor.light.primary),
+        overlayColor: MaterialStateProperty.all(NutriByteColor.light.primary),
+      ),
+    ),
+  );
 
   static ThemeData darkTheme = ThemeData(
       useMaterial3: true,
@@ -27,6 +148,5 @@ class NutriByteTheme {
           onTertiary: NutriByteColor.dark.onTertiary,
           tertiaryContainer: NutriByteColor.dark.tertiaryContainer,
           onTertiaryContainer: NutriByteColor.dark.onTertiaryContainer,
-          brightness: Brightness.dark)
-      );
+          brightness: Brightness.dark));
 }
--- frontend/lib/app/modules/auth/login/views/login_view.dart
+++ frontend/lib/app/modules/auth/login/views/login_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
+import 'package:nutri_byte/app/core/theme/colors.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -9,15 +11,146 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      backgroundColor: Get.theme.colorScheme.primaryContainer,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          SvgPicture.asset(
+            'assets/images/nutribyte.svg',
+            width: context.width * 0.3,
+            height: context.width * 0.3,
+          ),
+          SizedBox(
+            height: 55,
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 48),
+              decoration: BoxDecoration(
+                color: Get.theme.colorScheme.background,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Log in',
+                    style: Get.textTheme.displaySmall!
+                        .copyWith(color: Get.theme.colorScheme.surfaceTint),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration:
+                        InputDecoration(hintText: 'Email', labelText: 'Email'),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      labelText: 'Password',
+                      suffixIcon: Icon(Icons.visibility),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password?',
+                      style: Get.textTheme.titleMedium,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: NutriByteColor.primary70,
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                      ),
+                      child: Text(
+                        'Log In',
+                        style: Get.textTheme.titleLarge!
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Divider(
+                        color: NutriByteColor.darkText,
+                        height: 1,
+                      )),
+                      Text(
+                        'or sign in using',
+                        style: TextStyle(
+                            color: NutriByteColor.darkText, fontSize: 14),
+                      ),
+                      Expanded(
+                          child: Divider(
+                        color: NutriByteColor.darkText,
+                        height: 1,
+                      )),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    child: SvgPicture.asset(
+                      'assets/images/circle_google.svg',
+                      width: 48,
+                      height: 48,
+                    ),
+                    shape: CircleBorder(side: BorderSide.none),
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: Get.textTheme.bodyMedium!
+                            .copyWith(color: Get.theme.colorScheme.surfaceTint),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Sign Up',
+                          style: Get.textTheme.bodyMedium!
+                              .copyWith(color: NutriByteColor.primary70),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
