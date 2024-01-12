5bf3dfe0d936c513431bbcb3d483e6a5e53c82a7
Xrayya <nur.alkhiyar@gmail.com>
2024-01-12 21:37:46 +0700

    style: add comma after last widget parameter again

--- frontend/lib/app/modules/home/detail_nutrition/views/detail_nutrition_view.dart
+++ frontend/lib/app/modules/home/detail_nutrition/views/detail_nutrition_view.dart
@@ -22,69 +22,70 @@ class DetailNutritionView extends GetView<DetailNutritionController> {
   const DetailNutritionView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Card(
-            elevation: 5,
-            margin: EdgeInsets.all(0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-            ),
-            color: Theme.of(context).colorScheme.primaryContainer,
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: FloatingActionButton(
-                      child: Icon(Icons.arrow_back),
-                      onPressed: () {},
-                      shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(9999))),
-                    ),
-                  ),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              elevation: 5,
+              margin: EdgeInsets.all(0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
                 ),
-                SfCircularChart(
-                  // TODO: custom legend
-                  legend: Legend(
-                    isVisible: true,
-                    position: LegendPosition.bottom,
-                    orientation: LegendItemOrientation.vertical,
-                    height: '100%',
-                  ),
-                  borderWidth: 1,
-                  series: [
-                    RadialBarSeries<ChartData, String>(
-                      dataSource: pieData,
-                      xValueMapper: (ChartData data, _) => data.nutritionName,
-                      yValueMapper: (ChartData data, _) => data.percentage,
-                      dataLabelMapper: (ChartData data, _) => data.text,
-                      cornerStyle: CornerStyle.bothCurve,
-                      gap: '10%',
-                      radius: '100%',
-                      maximumValue: 100,
-                    ),
-                  ],
-                  annotations: [
-                    CircularChartAnnotation(
-                      height: '90%',
-                      width: '90%',
-                      widget: CircleAvatar(
-                        backgroundImage: AssetImage('images/placeholder.png'),
+              ),
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: FloatingActionButton(
+                        child: Icon(Icons.arrow_back),
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(9999))),
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SfCircularChart(
+                    // TODO: custom legend
+                    legend: Legend(
+                      isVisible: true,
+                      position: LegendPosition.bottom,
+                      orientation: LegendItemOrientation.vertical,
+                      height: '100%',
+                    ),
+                    borderWidth: 1,
+                    series: [
+                      RadialBarSeries<ChartData, String>(
+                        dataSource: pieData,
+                        xValueMapper: (ChartData data, _) => data.nutritionName,
+                        yValueMapper: (ChartData data, _) => data.percentage,
+                        dataLabelMapper: (ChartData data, _) => data.text,
+                        cornerStyle: CornerStyle.bothCurve,
+                        gap: '10%',
+                        radius: '100%',
+                        maximumValue: 100,
+                      ),
+                    ],
+                    annotations: [
+                      CircularChartAnnotation(
+                        height: '90%',
+                        width: '90%',
+                        widget: CircleAvatar(
+                          backgroundImage: AssetImage('images/placeholder.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
+            Padding(
               padding: const EdgeInsets.all(27.0),
               child: Card(
                 elevation: 5,
@@ -108,8 +109,10 @@ class DetailNutritionView extends GetView<DetailNutritionController> {
                         .toList(),
                   ),
                 ),
-              ))
-        ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
