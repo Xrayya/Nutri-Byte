a576cfe8587c8101267160f48fed71265969c206
Xrayya <nur.alkhiyar@gmail.com>
2024-01-18 00:04:53 +0700

    feat: slice quest page

--- frontend/lib/app/modules/home/quests/views/quests_view.dart
+++ frontend/lib/app/modules/home/quests/views/quests_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:nutri_byte/app/widgets/nutribyte_back_button.dart';
 
 import '../controllers/quests_controller.dart';
 
@@ -9,14 +10,50 @@ class QuestsView extends GetView<QuestsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QuestsView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'QuestsView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const NutriByteBackButton(),
+            const Text(
+              "Today's Quest",
+              style: TextStyle(
+                color: Color(0xFF294B19),
+                fontWeight: FontWeight.w600,
+                fontSize: 28,
+              ),
+            ),
+            Obx(
+              () => Column(
+                children: controller.quests
+                    .toList()
+                    .map((quest) => Card(
+                          margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                          clipBehavior: Clip.hardEdge,
+                          elevation: 4,
+                          child: ListTile(
+                            title: Text(
+                              quest.title,
+                              style: Get.textTheme.titleLarge!.copyWith(
+                                color: Get.theme.colorScheme.primary,
+                              ),
+                            ),
+                            subtitle: Text(
+                              "${quest.coin} NutriCoins",
+                              style: Get.textTheme.bodyLarge!.copyWith(
+                                color: Get.theme.colorScheme.primary,
+                              ),
+                            ),
+                            // TODO: find the correct icon
+                            trailing: Icon( quest.completed ? Icons.verified_outlined : Icons.radio_button_off_outlined,
+                              size: 42,
+                            ),
+                            tileColor: Get.theme.colorScheme.primaryContainer,
+                          ),
+                        ))
+                    .toList(),
+              ),
+            )
+          ],
         ),
       ),
     );
